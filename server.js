acf2eb8c964a9b1fd481c66af120b2814209631a
Alec Maier <Maier.144@osu.edu>
2019-01-22 16:39:08 -0500

    create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
